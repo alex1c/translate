1cf4409528f39ceb3b0ab718bc50053aa851f3fa
alex1c <alex1c.spb@gmail.com>
2022-03-15 11:50:18 +0300

    master

--- src/components/SelectLanguage/SelectLanguage.jsx
+++ src/components/SelectLanguage/SelectLanguage.jsx
@@ -5,114 +5,94 @@ import { useState, useEffect } from "react";
 var axios = require("axios").default;
 
 var options = {
-  method: "GET",
-  url: "https://deep-translate1.p.rapidapi.com/language/translate/v2/languages",
-  headers: {
-    "x-rapidapi-host": "deep-translate1.p.rapidapi.com",
-    "x-rapidapi-key": "9352ec2060mshbb3c9c44fb8d69bp1c139djsn9bdf0bfb4f0d",
-  },
+    method: "GET",
+    url: "https://deep-translate1.p.rapidapi.com/language/translate/v2/languages",
+    headers: {
+        "x-rapidapi-host": "deep-translate1.p.rapidapi.com",
+        "x-rapidapi-key": "9352ec2060mshbb3c9c44fb8d69bp1c139djsn9bdf0bfb4f0d",
+    },
 };
 
 function SelectLanguage(props) {
-  const [appState, setAppState] = useState();
+    const [appState, setAppState] = useState();
 
-  let triggerMap = [];
-
-  //дефолтные значения
-  if (props.direction === "from") {
-    localStorage.setItem("languageFrom", "ru");
-  } else if (props.direction === "to") {
-    localStorage.setItem("languageTo", "en");
-  }
-  localStorage.setItem("text", "Привет мир");
-
-  //получаем данные
-  useEffect(() => {
-    axios
-      .request(options)
-      .then(function (response) {
-        //console.log("000 ", response.data);
-        setAppState(response.data);
-        return response.data;
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-  }, []);
-
-  //обработчик клика по одному языку
-  function handlerLanguageClick(lang) {
-    const arraywithSelectLanguage = arrayAllLanguagesLocal.map(updateColor);
-
-    function updateColor(val) {
-      val.cl = "notActive";
-      if (val.name === lang) {
-        val.cl = "";
-
+    //дефолтные значения
+    useEffect(() => {
         if (props.direction === "from") {
-          localStorage.setItem("languageFrom", val.language);
+            localStorage.setItem("languageFrom", "ru");
         } else if (props.direction === "to") {
-          localStorage.setItem("languageTo", val.language);
+            localStorage.setItem("languageTo", "en");
         }
-      }
+        localStorage.setItem("text", "Привет мир");
+    }, []);
 
-      return val;
-    }
+    //получаем данные
+    useEffect(() => {
+        axios
+            .request(options)
+            .then(function (response) {
+                //console.log("000 ", response.data);
+                console.log(response.data.languages);
+                setAppState(
+                    response.data.languages.map((el) => ({
+                        ...el,
+                        cl: "notActive",
+                    }))
+                );
+                return response.data;
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+    }, []);
 
-    //помещаем массив с выбранным языком в стейт (заменяем)
-    setAllLanguagesLocal(arraywithSelectLanguage);
-  }
+    //обработчик клика по одному языку
+    function handlerLanguageClick(lang) {
+        const arraywithSelectLanguage = appState.map(updateColor);
 
-  const [arrayAllLanguagesLocal, setAllLanguagesLocal] = useState([]);
-  var arrayAllLanguages = [];
+        function updateColor(val) {
+            val.cl = "notActive";
+            if (val.name === lang) {
+                val.cl = "";
 
-  //перебираем полученные данные и заполняем локальный массив
-  function setAllLanguages() {
-    //console.log(appState)
-
-    for (var key in appState) {
-      appState[key].map((t) => arrayAllLanguages.push(t));
-      arrayAllLanguages.map((v) => (v.cl = "notActive")); //добавляем класс не активности по умолчанию
-    }
-
-    triggerMap.push("1");
-
-    return arrayAllLanguages;
-  }
-  setAllLanguages();
-
-  //после заполнения локального массива переносим его в стейт
-  useEffect(() => {
-    //без условия на заполненность код ниже срабатывал постоянно
-    if (arrayAllLanguagesLocal.length === 0) {
-      setAllLanguagesLocal(arrayAllLanguages);
-    }
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [triggerMap]); //arrayAllLanguages
-  //когда здесь был arrayAllLanguages то шел постоянный ререндеринг. Сделал служебный триггерный массив. Он дергается однократно в конце перебора основного массива
-
-  return (
-    <>
-      <div>{<h1 className={classes.SelectLanguage}>{props.direction}</h1>}</div>
-      <div className={classes.blocklanguages}>
-        {arrayAllLanguagesLocal.map((p) => (
-          <div
-            key={p.name}
-            id={p.name}
-            onClick={(e) => handlerLanguageClick(e.target.id)}
-            className={
-              p.cl === "notActive"
-                ? classes.oneLanguage
-                : classes.oneLanguageActive
+                if (props.direction === "from") {
+                    console.log(val.language);
+                    localStorage.setItem("languageFrom", val.language);
+                } else if (props.direction === "to") {
+                    localStorage.setItem("languageTo", val.language);
+                }
             }
-          >
-            {" " + p.name + " (" + p.language + ") "}
-          </div>
-        ))}
-      </div>
-    </>
-  );
+            return val;
+        }
+
+        //помещаем массив с выбранным языком в стейт (заменяем)
+        setAppState(arraywithSelectLanguage);
+    }
+
+    return (
+        <>
+            <div>
+                {<h1 className={classes.SelectLanguage}>{props.direction}</h1>}
+            </div>
+            <div className={classes.blocklanguages}>
+                {appState &&
+                    appState.map((p) => (
+                        <div
+                            key={p.name}
+                            id={p.name}
+                            onClick={(e) => handlerLanguageClick(e.target.id)}
+                            className={
+                                p.cl === "notActive"
+                                    ? classes.oneLanguage
+                                    : classes.oneLanguageActive
+                            }
+                        >
+                            {" " + p.name + " (" + p.language + ") "}
+                        </div>
+                    ))}
+            </div>
+        </>
+    );
 }
 
 export default SelectLanguage;
